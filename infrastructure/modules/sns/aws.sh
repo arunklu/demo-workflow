419a6958254f387010ecbb21588ed7e480b60a27
arunklu <devops@kludgenetworks.in>
2023-06-05 23:53:59 +0530

    removed

--- infrastructure/modules/sns/aws.sh
+++ infrastructure/modules/sns/aws.sh
@@ -1,3 +1,3 @@
 
-export AWS_ACCESS_KEY_ID= ${{secrets.AWS_ACCESS_KEY_ID }}
-export AWS_SECRET_ACCESS_KEY= ${{secrets.AWS_ACCESS_KEY_ID }}
+# export AWS_ACCESS_KEY_ID= ${{secrets.AWS_ACCESS_KEY_ID }}
+# export AWS_SECRET_ACCESS_KEY= ${{secrets.AWS_ACCESS_KEY_ID }}
