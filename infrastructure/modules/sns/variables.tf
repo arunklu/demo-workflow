acb24972bec3f12280960e4c45d7585280fd3941
arunklu <devops@kludgenetworks.in>
2023-06-05 23:39:10 +0530

    region

--- infrastructure/modules/sns/variables.tf
+++ infrastructure/modules/sns/variables.tf
@@ -2,3 +2,12 @@ variable "sns_topics" {
   default = ["patient-topic", "patient-linking-topic", "screening-topic", "evoyareport-topic", "event-history-topic", "outbox-topic"]
 }
 
+variable "access_key" {
+  default = "AKIA2OPKKENWARYWZ4FP"
+  
+}
+
+variable "secret_key" {
+  default = "QxJiyESAA7dsI2ysBCbMY2AkKiiL/Gm4rJMMv35X"
+  
+}
