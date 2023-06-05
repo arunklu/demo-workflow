9ee07dbf622f88f3cbc33cc6707207abdf0b82e4
arunklu <devops@kludgenetworks.in>
2023-06-05 23:42:42 +0530

    ${{secrets.AWS_ACCESS_KEY_ID }}

--- infrastructure/modules/sns/aws.sh
+++ infrastructure/modules/sns/aws.sh
@@ -1,4 +1,3 @@
-AWS_ACCESS_KEY_ID="$secrets.AWS_ACCESS_KEY_ID"
-AWS_SECRET_ACCESS_KEY="$secrets.AWS_SECRET_ACCESS_KEY"
-export AWS_ACCESS_KEY_ID="anaccesskey"
-export AWS_SECRET_ACCESS_KEY="asecretkey"
+
+export AWS_ACCESS_KEY_ID= ${{secrets.AWS_ACCESS_KEY_ID }}
+export AWS_SECRET_ACCESS_KEY= ${{secrets.AWS_ACCESS_KEY_ID }}
